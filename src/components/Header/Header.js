e8eec8fd084b947ec452cef334d30d85247c46be
pinwei wu <pw.amy.wu@gmail.com>
2020-12-01 11:28:32 -0600

    add search and navigation component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navigation from '../Navigation/Navigation';
+import SearchForm from '../SearchForm/SearchForm';
 import './Header.css';
 
 export default function Header() {
     return (
-        <Header className="header">
-            <title>What's going on in the world?</title>
-        </Header>
+        <header className="header">
+            <Navigation />
+            <div className="header__title-box"><h1 className="header__title">What's going on in the&nbsp;world?</h1></div>
+            <div className="header__subtitle-box"><h3 className="header__subtitle">Find the latest news on any topic and save them in your personal account.</h3></div>
+            <SearchForm />
+        </header>
     )
 }
