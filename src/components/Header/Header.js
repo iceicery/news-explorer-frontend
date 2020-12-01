940cc8729ba6404d5b4f443db47bafe9ca7848a2
pinwei wu <pw.amy.wu@gmail.com>
2020-12-01 09:40:40 -0600

    Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <Header className="header">
+            <title>What's going on in the world?</title>
+        </Header>
+    )
+}
