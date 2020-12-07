f4f0c8bbd3a7c9099b40e4ddfca9f7063853eca2
pinwei wu <pw.amy.wu@gmail.com>
2020-12-07 14:45:52 -0600

    add NavPopup feature

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,10 +3,10 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import SearchNews from "../SearchNews/SearchNews";
 
-export default function Main({ handleSigninOpen, isLogin, handleLogout }) {
+export default function Main({ handleSigninOpen, isLogin, handleLogout, handleNavOpen }) {
     return (
         <section className="main">
-            <Header isLogin={isLogin} handleSigninOpen={handleSigninOpen} handleLogout={handleLogout} />
+            <Header isLogin={isLogin} handleSigninOpen={handleSigninOpen} handleLogout={handleLogout} handleNavOpen={handleNavOpen} />
             <SearchNews isLogin={isLogin} />
             <About />
             <Footer />
---
+++ src/components/NavPopup/NavPopup.js
@@ -0,0 +1,64 @@
+import "./NavPopup.css";
+import { Link } from "react-router-dom";
+import { useCallback, useEffect } from "react";
+import logoutIconLight from '../../images/logout-light.png';
+export default function NavPopup({ isOpen, isLogin, handleSigninOpen, handlePopupClose, handleLogout }) {
+    function onClickSignin() {
+        handlePopupClose();
+        handleSigninOpen();
+    }
+    function onClickClose() {
+        handlePopupClose();
+    }
+    function onClickLogout() {
+        handleLogout();
+    }
+
+    const escFunction = useCallback((event) => {
+        if (event.keyCode === 27) {
+            handlePopupClose();
+        }
+    }, [handlePopupClose]);
+    const tempName = "Elise";
+
+    useEffect(() => {
+        document.addEventListener("keydown", escFunction, false);
+
+        return () => {
+            document.removeEventListener("keydown", escFunction, false);
+        };
+    }, [escFunction]);
+
+    const overlayClass = isOpen ? "overlay" : "hidden";
+    const navpopupClass = isOpen ? "navpopup" : "hidden";
+
+    return (
+        <>
+            <div className={overlayClass}></div>
+            <nav className={navpopupClass}>
+                <div className="navpopup__container">
+                    <Link to="/" className="link"><p className="navpopup__title">NewsExplorer</p></Link>
+                    <button className="navpopup__close" onClick={onClickClose}></button>
+                </div>
+                <div className="navpopup__box">
+                    <Link to="/" className="link" onClick={onClickClose}>
+                        <p className="navpopup__place">Home</p>
+                    </Link>
+                    {isLogin &&
+                        <Link to="/saved-news" className="link" onClick={onClickClose}>
+                            <p className="navpopup__place">Saved articles</p>
+                        </Link>
+                    }
+                    {isLogin ?
+                        <button className="navpopup__button">
+                            <p className="nav__login-text">{tempName}</p>
+                            <Link to="/" className="link" onClick={onClickClose}><img src={logoutIconLight} alt="logout icon" className="nav__icon" onClick={onClickLogout} /></Link>
+                        </button> :
+                        <button className="navpopup__button" onClick={onClickSignin}>Sign In</button>
+                    }
+
+                </div>
+            </nav>
+        </>
+    )
+}
