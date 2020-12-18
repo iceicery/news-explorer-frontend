3ae73d3025fdbc73a675a6c1d0de638274738f67
pinwei wu <pw.amy.wu@gmail.com>
2020-12-18 13:07:23 -0600

    add errmassage while submit an empty search

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,24 +1,33 @@
+import { useState } from "react";
 import { useFormWithValidation } from "../../utils/FormValidation";
 import "./SearchForm.css";
 
 export default function SearchForm({ handleSearch, handleSearchSubmit, handleHindMore }) {
     const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation();
+    const [errMessage, setErrMessgae] = useState('')
+    console.log(isValid);
     function onChangeTopic(e) {
-        handleChange(e)
+        handleChange(e);
+        setErrMessgae('');
     }
     function onClickSubmit(e) {
         e.preventDefault();
+        if (!isValid) {
+            setErrMessgae("Please enter a keyword");
+            return;
+        }
         handleSearchSubmit(values.topic);
         handleSearch(values.topic);
         handleHindMore();
         resetForm();
     }
-    const diableButton = isValid ? false : true;
+
     return (
         <form className="search">
             <input className="search__input" placeholder="Enter topic" onChange={onChangeTopic} name="topic" required />
             <span className="search__err">{errors.topic}</span>
-            <button className="search__button" disabled={diableButton} onClick={onClickSubmit}>Search</button>
+            <button className="search__button" onClick={onClickSubmit}>Search</button>
+            <span className="search__err-click">{errMessage}</span>
         </form>
     )
 }
