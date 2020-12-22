0fcdb449ac192d78142976f9553c959f5ae6825a
pinwei wu <pw.amy.wu@gmail.com>
2020-12-22 09:58:11 -0600

    fix:The date parameters of requests to News API must be calculated automatically

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,19 +1,14 @@
 export default class NewsApi {
     constructor({ base_url, apiKey, from, to, pageSize }) {
         this.url = base_url;
-        this.apikey = apiKey;
+        this.apiKey = apiKey;
         this.from = from;
         this.to = to;
         this.pageSize = pageSize;
     }
 
     requeireNews(request) {
-        const apiKey = "61a2215ddb5049b9a608970770a757dc";
-        const from = (new Date((Date.now() - 7 * 24 * 60 * 60 * 1000))).toString().slice(0, 10);
-        const to = (new Date(Date.now())).toString().slice(0, 10);
-        const pageSize = 100;
-        const url = 'https://nomoreparties.co/news/v2/everything?';
-        return fetch(`${url}q=${request}&from=${from}&to=${to}&pageSize=${pageSize}&apiKey=${apiKey}`, {
+        return fetch(`${this.url}q=${request}&from=${this.from}&to=${this.to}&pageSize=${this.pageSize}&apiKey=${this.apiKey}`, {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json"
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,17 +3,23 @@ import MainApi from "./MainApi";
 
 const NEWS_URL = 'https://nomoreparties.co/news/v2/everything?';
 const APIKEY = "61a2215ddb5049b9a608970770a757dc";
-const FROM = (new Date((Date.now() - 7 * 24 * 60 * 60 * 1000))).toString().slice(0, 10);
-const TO = (new Date(Date.now())).toString().slice(0, 10);
+const month = {
+    Jan: "01", Feb: "02", Mar: "03", Apr: "04", May: "05", Jun: "06",
+    Jul: "07", Aug: "08", Sep: "09", Oct: "10", Nov: "11", Dec: "12"
+};
+const FROM = (new Date((Date.now() - 7 * 24 * 60 * 60 * 1000))).toString().slice(0, 15);
+const FROM_FORMAT = `${FROM.slice(11, 15)}-${month[FROM.slice(4, 7)]}-${FROM.slice(8, 10)}`;
+const TO = (new Date(Date.now())).toString().slice(0, 15);
+const TO_FORMAT = `${TO.slice(11, 15)}-${month[TO.slice(4, 7)]}-${TO.slice(8, 10)}`
 const PAGESIZE = 100;
 const MAIN_URL = 'https://api.newsfinder.students.nomoreparties.site';
 
 export const newsApi = new NewsApi({
     base_url: NEWS_URL,
-    APIKEY,
-    FROM,
-    TO,
-    PAGESIZE,
+    apiKey: APIKEY,
+    from: FROM_FORMAT,
+    to: TO_FORMAT,
+    pageSixe: PAGESIZE,
 })
 
 export const mainApi = new MainApi(MAIN_URL);
